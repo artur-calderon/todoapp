dbb8319416f32d1217b590a2ac6941313388611c
Artur Calderon <86498045+artur-calderon@users.noreply.github.com>
2022-10-15 20:48:45 -0400

    Responsive and functionality

--- src/components/Main/Main.tsx
+++ src/components/Main/Main.tsx
@@ -3,27 +3,81 @@ import styles from './styles.module.css'
 import { AiOutlinePlusCircle } from 'react-icons/ai'
 import { BsClipboardX } from 'react-icons/bs'
 import { Task } from '../Tasks/Task'
+import { FormEvent, useState } from 'react'
 
 export function Main() {
+    const [tasks, setTasks] = useState<string[]>([])
+    const [newTask, setNewTask] = useState('')
+    const[taskConcludedCount, setTaskConcludedCount]= useState(0)
+
+
+
+    function handleNewTask(event: FormEvent) {
+        event.preventDefault()
+        setTasks([...tasks, newTask])
+        setNewTask('')
+    }
+
+    // function handleNewTaskText(event:ChangeEvent<HTMLInputElement>){
+    //    setNewTask(event.target.value)
+    // }
+
+    function handleDeleteTask(taskToDelete:string, concludedOnDelete:boolean) {
+        const taskWithDeletedOne = tasks.filter(task=>{
+            return task != taskToDelete
+        });
+        if(concludedOnDelete){
+            setTaskConcludedCount(prev => prev-=1)
+        }
+        setTasks(taskWithDeletedOne)
+    }
+
+    function tasksConcludedCount(concluded:boolean){
+        if(concluded){
+            setTaskConcludedCount(prev => prev+=1)
+        }else if(taskConcludedCount <= 0){
+            return
+        }
+        else{
+            setTaskConcludedCount(prev => prev-=1)
+        }
+    }
+
     return (
         <main className={styles.main}>
-            <div className={styles.boxAdd}>
-                <input type="text" placeholder='Adicione uma nova tarefa' />
-                <button>Criar <AiOutlinePlusCircle size={16} /></button>
-            </div>
+            <form className={styles.boxAdd} onSubmit={handleNewTask}>
+                <input type="text" onChange={(e) => setNewTask(e.target.value)} value={newTask} placeholder='Adicione uma nova tarefa' />
+                <button type='submit'>Criar <AiOutlinePlusCircle size={16} /></button>
+            </form>
             <div className={styles.taskBoxHeader}>
-                <span className={styles.created}>Tarefas criadas <span>0</span></span>
-                <span className={styles.completed}>Concluídas <span>0</span></span>
+                <span className={styles.created}>Tarefas criadas <span>{tasks.length}</span></span>
+                <span className={styles.completed}>Concluídas <span>{taskConcludedCount} de {tasks.length}</span></span>
             </div>
-            {/* <div className={styles.taskBoxEmpty}>
-                <BsClipboardX/>
-                <p><span>Você ainda não tem tarefas cadastradas<br/></span>
-                Crie tarefas e organize seus itens a fazer</p>
-            </div> */}
-            <div className={styles.taskBox}>
-                <Task id='estudar' content='Estudar React' />
-                <Task id='prova' content='Prova' />
-            </div>
+            {
+                tasks.length == 0 ? (
+                    <div className={styles.taskBoxEmpty}>
+                        <BsClipboardX />
+                        <p><span>Você ainda não tem tarefas cadastradas<br /></span>
+                            Crie tarefas e organize seus itens a fazer</p>
+                    </div>
+                ) : (
+                    <div className={styles.taskBox}>
+                        {
+                            tasks.map(task => {
+                                return <Task
+                                    key={task}
+                                    id={task}
+                                    content={task}
+                                    onDeleteTask={handleDeleteTask}
+                                    tasksConcludedCount={tasksConcludedCount}
+                                />
+                            })
+                        }
+                    </div>
+                )
+            }
+
+
         </main>
     )
 }
--- src/components/Tasks/Task.tsx
+++ src/components/Tasks/Task.tsx
@@ -1,25 +1,37 @@
 
 import styles from './styles.module.css'
 import { BiTrash } from 'react-icons/bi'
-import { BsCheck } from 'react-icons/bs'
+import { BsCheck, BsInfoCircleFill } from 'react-icons/bs'
 import { useState } from 'react';
 
 interface Tasks {
     id: string;
     content: string;
+    onDeleteTask: (task:string, concludedOnDelete:boolean)=> void;
+    tasksConcludedCount:(concluded:boolean)=>void;
 }
 
-export function Task({ id, content }: Tasks) {
+export function Task({ id, content, onDeleteTask,tasksConcludedCount }: Tasks) {
     const [taskConcluded, setTaskConcluded] = useState(false)
+
+
+    function handledeleteTask(id:string){
+        onDeleteTask(id, taskConcluded)
+    }
+
+    function handleTaskConcluded(){
+        tasksConcludedCount(!taskConcluded)
+        setTaskConcluded(!taskConcluded)
+    }
 
     return (
         <div className={styles.taskContainer}>
             <label htmlFor={id} >
-                <input type="checkbox" onClick={() => setTaskConcluded(!taskConcluded)} className={styles.checkBox} name="" id={id} />
+                <input type="checkbox" onClick={handleTaskConcluded} className={styles.checkBox} name="" id={id} />
                 <BsCheck />
             </label>
-            <p className={taskConcluded ? styles.taskConcluded : 'null'}>{content}</p>
-            <BiTrash />
+            <p className={taskConcluded ? styles.taskConcluded : ''}>{content}</p>
+            <BiTrash onClick={()=>handledeleteTask(id)}/>
         </div>
     )
 }
